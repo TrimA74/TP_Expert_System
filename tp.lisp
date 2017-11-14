1cfe0f368ef2508ca22667d98e2d106d1a4ee8b3
bruhla <anthony.bruhl@laposte.net>
2017-11-14 17:26:34 +0100

    fact base and stop condition (intermediate commit)

--- tp.lisp
+++ tp.lisp
@@ -1,91 +1,68 @@
-(defun longueur (l)
-	(cond
-			((null l) 0 ) ; si l est null on renvoie 0
-			((atom l) nil) ; si l est un atom on renvoie nil
-			( t ( + (longueur ( cdr l)) 1 )) ; sinon on renvoie longueur du cdr de la liste + 1 
-	)
+;fact base
+
+
+(defstruct waste
+  	material
+  	volume
+  	dirt_type
 )
 
-(defun longueurPlus (l)
-	(cond
-			((null l) 0 )
-			((atom l) nil)
-			( (listp ( car l)) ( + (longueurPlus (car l) (longueurPlus (cdr l) )) ) ) ; si car l est une liste on renvoie la longueur de car + cdr de l 
-			( t ( + (longueur ( cdr l)) 1 ))
-	)
+(defstruct selectiveTrash 
+  	container_list
+    
 )
 
-(defun isIn (l e) 
-	(cond 
-		((atom l ) nil)
-		((equal e (car l)) t) ; si e = car l 
-		(t (isIn (cdr l) e)) ; sinon isIn 
-	)
+(defstruct container
+	color 
+   	max_capacity
+   	waste_amount
+   	authorized_material_list
+   	unauthorized_dirt_list
 )
 
-(defun munion ( l1 l2 )
-	(cond
-		((null l2) l1)
-		((null l1) l2)
-		((atom l2) (cond 
-						( (isIn l1 l2) l1)
-						(t append l1 l2)
-					))
-		((atom l1) (cond 
-						( (isIn l2 l1) l2)
-						(t append l2 l1
-					)))
-		((isIn l1 (car l2)) (munion l1 (cdr l2)) )
-		(t (cons (car l2) (munion (cdr l2) l1 )) )
-	)
-)
-; marche pas 
-(defun inter ( l1 l2 )
-	(cond
-		( (not (and  (listp l1) (listp l2) ) ) nil ) 
-		( (null l1 ) nil)
-		( ( member l1 (car l2) ) ( cons (car l2) (inter l1 (cdr l2) ) ) )
-		( t (inter l1 (cdr l2) ) ) 
-	)
-)
-(defun my-intersection ( liste1 liste2 )
-  (cond
-       ( (not (and (listp liste1) (listp liste2))) nil)
-       ( (null liste1) nil)
-       ( (member (car liste1) liste2) ( cons (car liste1) (my-intersection (cdr liste1) liste2) ))
-       (t (my-intersection (cdr liste1) liste2))
-  )
-)
-(defun fin (l)
-	(cond
-		((null l) nil )
-		( (null (cdr l) ) (car l) )
-		( t (fin (cdr l)) )
-	) 
+( setq green_container (make-container
+	:color "green"
+	:max_capacity "100"
+   	:waste_amount "0"
+   	:authorized_material_list ("glass")
+   	:unauthorized_dirt("chimic liquid","grease")
+   	)
 )
 
-(defun inverse (l)
-	(cond
-		( () () )
-		(t () )
-
-
-	)
-
-
+( setq yellow_container (make-container
+	:color "yellow"
+	:max_capacity "100"
+   	:waste_amount "0"
+   	:authorized_material_list ("carton","metal","plastic")
+   	:unauthorized_dirt("chimic liquid","grease")
+   	)
 )
 
-(defun renverse (liste)
-	(cond 
-		( (atom liste) liste)
- 	( t (append (renverse (cdr liste))
- 		(list (renverse (car liste)))
+( setq blue_container (make-container
+	:color "blue"
+	:max_capacity "100"
+   	:waste_amount "0"
+   	:authorized_material_list ("paper")
+   	:unauthorized_dirt()
+   	)
+)
 
- 	))
-
-
-
- 	)
-
-
+( setq waste (make-waste 
+	:material "Plastic"
+   	:volume "1"
+   	:dirt_type nil 
+   	)
 ) 
+
+;stop condition
+(setq stopCondition
+	(cond
+		
+		((or (>= green_container.max_capacity green_container.waste_amount)
+			(>= blue_container.max_capacity blue_container.waste_amount)
+			(>= yellow_container.max_capacity yellow_container.waste_amount)
+		)t)
+		
+
+	)
+)
