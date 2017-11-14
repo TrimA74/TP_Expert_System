f5b35ecdc0f5f8f23dc56f9cc229841551f5da26
TrimA <florianseb96@hotmail.fr>
2017-11-14 14:40:12 +0100

    ajout des fonctions pour le tp

--- tp.lisp
+++ tp.lisp
@@ -1,5 +1,104 @@
 ;SEBIRE branche
 
+
+
+(defun put(symbol prop value) ; ajouter une propriété dans un objet 'symbol'
+	(self (get symbol prop) value))
+; création d'une règle
+(defun newrule (symb cond act)
+	(cond ( (member symb LR) (print ‘Already_used_name) nil) ; si la rule est dans LR on fait rien
+		( t (setq LR (cons symb LR)) ; sinon on l'ajoute symb dans LR
+			(put symb ‘condition cond) ; ajouter une propriété appellée : 'condition' dans lequel on met cond
+			(put symb ‘action act) ; pareil
+			(put symb ‘appliquee nil) ; pareil
+			symb ))) ; return la rule (object symb)
+; suppresion d'une règle
+(defun deleterule (symb)
+	(cond ( (member symb LR) (setq LR (remove symb LR)) symb) ; si 
+		( t (print ‘The_rule_does_not_exist) nil))) 
+
+; on affiche une règle
+(defun printrule (symb)
+(cond ( (null (member symb LR)) (print ‘The_rule_does_notè_exist) nil)
+	( t (print symb)
+	(prin1 ‘ condition :’)(print (get symb condition))
+	(prin1 ‘ action :’)(print (get symb action))
+	symb ))) 
+; Tester si une règle est applicable 
+(defun applicable (x)
+ (eval (get x ‘condition ) )) 
+
+; Appliquer une règle 
+ $ (defun appliquer (x)
+ (eval (get x ‘action))
+ (put x ‘appliquee t) ) 
+
+ ;Savoir si une règle a déjà été appliquée
+ (defun appliquee (x)
+ 	(get x ‘appliquee) ) 
+; Ensemble de conflits (liste des règles activables) 
+ (defun conflictSet (ll) ; ll variable locale
+ (setq ll nil)
+(mapc ‘(lambda (x) (cond ( (and (not (appliquee x)) (applicable x))
+ (setq ll (cons x ll)) ) ))
+ LR)
+ll ) 
+; running expert system
+ (defun run ()
+ (cond ( (eval CA) (print ‘Fin_Condition_d_arret_verifiée) t )
+ ( (null (conflictset)) (print ‘Pb_non_resolu_et_plus_de_Regles_Activables) nil )
+( t (mapc ‘appliquer (conflictset)) ; on applique toutes les règles
+ (run) ))) 
+
+
+; Exemple
+; ------------
+; Base de faits
+; ------------------
+(setq Temp 37)
+(setq fortefievre nil)
+; Base de règles
+; ---------------------
+( setq LR nil ) ; LR liste des règles 
+(newrule 'r1 '(> Temp 38) '(setq fortefievre T))
+(newrule 'r2 T ’(progn (prin1 'temperature?) (setq Temp (read))))
+; Condition d arrêt
+; ------------------------
+(setq CA '(not (null fortefievre)))
+; Lancement du moteur
+; -------------------------------
+(run) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (defun longueur (l)
 	(cond
 			((null l) 0 ) ; si l est null on renvoie 0
