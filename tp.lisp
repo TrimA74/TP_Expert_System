28e926f485d78a8241f63d9078f444efc9868ae6
olivier <olivier.sthioul@etu.univ-savoie.fr>
2017-11-30 18:23:43 +0100

    bloqué au faite d'identifier la r1

--- tp.lisp
+++ tp.lisp
@@ -18,20 +18,24 @@
   waste_amount
  )
 
+;rule base
+
+;init variables
+(defvar green_container)
 ( setq green_container (make-container
   	:color '"green"
   	:max_capacity 100
     :waste_amount 0
   )
 )
-
+(defvar yellow_container)
 ( setq yellow_container (make-container
   	:color '"yellow"
   	:max_capacity 100
     :waste_amount 0
   )
 )
-
+(defvar blue_container)
 ( setq blue_container (make-container
   	:color '"blue"
   	:max_capacity 100
@@ -39,21 +43,28 @@
 
   )
 )
-
-( setq waste (make-waste 
-  	:material '"Plastic"
+(defvar waste1)
+( setq waste1 (make-waste 
+  	:material 'paper
     :volume 1
-    :dirt_type nil 
+    :dirt_type 'clean
   )
 ) 
 
+(defvar containerChosen)
+(setq containerChosen nil)
+(defvar stopCondition)
+(defvar rulesList)
+
 
 ;Rule list
-(defvar LR '())
+(defvar LR)
+(setq LR nil)
+
+
 
 ; Définition des structures
-(defstruct rule name condition action weight)
-(defstruct fact name attributes is_a)
+(defstruct rule name condition action weight applied)
 
 ; Fonctions de manipulations
 (defun newrule(myname mycondition myaction myweight)
@@ -62,29 +73,46 @@
                :condition mycondition
                :action myaction
                :weight myweight
+               :applied nil
           )
     LR)
 )
 
+;(defun put (symbol prop value)     ;marche pas
+;    (setf (get symbol prop) value))    
+
+(defun myApply (x) ;@x une règle
+    (eval (rule-action x)) ; on évalue l'action de x
+    (setf (rule-applied x) t) ;(put x 'applied t) ) ; mets true dans la propriété appliquee de @x
+)
+
 ; Tester si une règle est applicable 
 (defun applicable (x)
-    (eval (get x 'condition ) )) ; on évalue la condition  retourn bool
+    (print (rule-condition x))
+    (eval (rule-condition x))                                   ;(get x 'condition ) )) ; on évalue la condition  retourn bool
+)    
 
 ;Savoir si une règle a déjà été appliquée
-(defun appliquee (x)
-    (get x 'appliquee) ) ; récupère la propriété appliquée
+(defun applied (x)
+    (rule-applied x)
+    ;(get x 'applied) 
+) ; récupère la propriété appliquée
 
 ; findActivable
 (defun findActivable ()
         (setq rulesList nil)
         (mapc 
             (lambda (x) 
+                (print x)
+                (print (applicable x))
                 (cond
-                    (and (not (null (appliquee x))) applicable x)
-                    (setq rulesList (cons x rulesList))
+                    ((and (not (null (applied x))) (applicable x))
+                    (setq rulesList (cons x rulesList)))
                 )
+                
             )
         LR)
+        (print rulesList)
         rulesList
 )
 ; adding rules
@@ -93,28 +121,27 @@
 ;; condition -> si waste.material == glass 
 ;; action -> null
 ;; weight 1
-(newrule 'r1 '(eq  (waste-material waste) "Paper") '(print ""))
+(newrule 'r1 '(eq  (waste-material waste1) 'paper) '(print "ok") 1)
 
 ; r2
-;; condition -> si waste.dirt_type == clean && r1.appliquee==true
+;; condition -> si waste.dirt_type == clean && r1.applied==true
 ;; action -> mettre dans la glassTrash
 ;; weight 13
-(newrule 'r2 '(eq  (and (eq (waste-dirt_type waste) "clean" ) r1 appliquee)) '(setq containerChoosen "blue") )
+(newrule 'r2 '(and (eq (waste-dirt_type waste1) 'clean ) (applied r1)) '(setq containerChosen 'blue_container) 1)
 
 
 
-; CA -> trahs are full or no waste to add
 
-(setq CA t)
+
 
 ;stop condition
 (setq stopCondition
 	(cond
 		
-		((or (>= (container-max_capacity green_container) (container-waste_amount green_container))
-			(>= (container-max_capacity blue_container) (container-waste_amount blue_container))
-			(>= (container-max_capacity yellow_container) (container-waste_amount yellow_container))
-            (not (null containerChoosen))  ;on s'arrete quand la poubelle choisi est défini
+		((or (<= (container-max_capacity green_container) (container-waste_amount green_container))
+			(<= (container-max_capacity blue_container) (container-waste_amount blue_container))
+			(<= (container-max_capacity yellow_container) (container-waste_amount yellow_container))
+            (not (null containerChosen))  ;on s'arrete quand la poubelle choisi est défini
                                             ;poubelle peut etre blue, yellow, green...ou other(aucune de nos poubelles correspondent)
 		)t)
 		
@@ -127,12 +154,12 @@
 ; -----------------------
 (defun run ()
     (cond 
-        ( (eval stopCondition) (print 'motor_stop) t )
+        ( (eval stopCondition) (print 'motor_stop) (print containerChosen ))
         ( 
-            (null (conflits)) (print 'problem_not_solve_or_nothing_applicable) nil  
+            (null (findActivable)) (print 'problem_not_solve_or_nothing_applicable) nil 
         )
         ( t 
-            (mapc 'appliquer (conflictset)) ; on applique toutes les règles
+            (mapc 'myApply (findActivable)) ; on applique toutes les règles
             (run)
         )
     )
